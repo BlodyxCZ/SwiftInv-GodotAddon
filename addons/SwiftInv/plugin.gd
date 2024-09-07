28d61a300c31cd5bb1d44827415f6386ae6610e2
BlodyxCZ <kjubercz@gmail.com>
2024-09-07 21:26:03 +0200

    Update plugin.gd

--- addons/SwiftInv/plugin.gd
+++ addons/SwiftInv/plugin.gd
@@ -4,9 +4,15 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
-	pass
+	add_custom_type("Inventory", "Resource", preload("res://addons/SwiftInv/Scripts/Inventory.gd"), preload("res://addons/SwiftInv/Icons/Inventory.svg"))
+	add_custom_type("InventoryContainer", "GridContainer", preload("res://addons/SwiftInv/Scripts/InventoryContainer.gd"), preload("res://addons/SwiftInv/Icons/InventoryContainer.svg"))
+	add_custom_type("InventoryItem", "Resource", preload("res://addons/SwiftInv/Scripts/InventoryItem.gd"), preload("res://addons/SwiftInv/Icons/InventoryItem.svg"))
+	add_custom_type("InventorySlot", "TextureRect", preload("res://addons/SwiftInv/Scripts/InventorySlot.gd"), preload("res://addons/SwiftInv/Icons/InventorySlot.svg"))
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
-	pass
+	remove_custom_type("Inventory")
+	remove_custom_type("InventoryContainer")
+	remove_custom_type("InventoryItem")
+	remove_custom_type("InventorySlot")
